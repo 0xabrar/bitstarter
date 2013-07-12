84d61b95ae5a842cadbc1b525425f1ba441aee22
Abrar Hussain <abrar.a.hussain@gmail.com>
2013-07-12 21:09:29 +0000

    Updated grader.js

--- grader.js
+++ grader.js
@@ -1,58 +1,41 @@
 #!/usr/bin/env node
 /*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
+Automatic grade files for the presence of specified HTML tags/attributes.
+Uses commander.js and cheerio. Teachers command line application development
+and basic DOM parsing
 
 References:
 
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
+	+ cheerio
+	   - https://github.com/MatthewMueller/cheerio
+	   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
+	   - http://maxogden.com/scraping-with-node.html
+	 
+	 + commander.js
+	   - https://github.com/visionmedia/commander.js
+	   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
 
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
+	 + JSON
+	   - http://en.wikipedia.org/wiki/JSON
+	   - https://developer.mozilla.org/en-US/docs/JSON
+	   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
 */
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
-    var instr = infile.toString();
-    if(!fs.existsSync(instr)) {
-        console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
-    }
-    return instr;
-};
-
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
-};
-
-var loadChecks = function(checksfile) {
-    return JSON.parse(fs.readFileSync(checksfile));
-};
-
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
-    var checks = loadChecks(checksfile).sort();
-    var out = {};
-    for(var ii in checks) {
-        var present = $(checks[ii]).length > 0;
-        out[checks[ii]] = present;
-    }
-    return out;
+	var instr = infile.toString();
+	if(!fs.existsSync(instr)){
+		console.log("%s does not exist. Exiting.", instr);
+		process.exit(1);
+		// http://nodejs.org/api/process.html#process_process_exit_code
+	}
+	return instr;
 };
 
 var clone = function(fn) {
@@ -61,14 +44,58 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+//Unused now
+var cheerioHtmlFile = function(htmlfile){
+	return cheerio.load(fs.readFileSync(htmlfile));
+};
+
+var loadChecks = function(checksfile) {
+	return JSON.parse(fs.readFileSync(checksfile));
+};
+
+var checkHtmlFile = function(htmlfile, checksfile){
+	$ = cheerio.load(htmlfile); //$ = cheerioHtmlFile(htmlfile);
+	var checks = loadChecks(checksfile).sort();
+	var out = {};
+	for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	}
+	return out;
+};
+
+var parseFile = function(checkHtmlFile){
+	
+	var response = function(result){
+	
+	};
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+		.option('-u, --url <url>', 'Webpage URL.')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+		
+	if(program.url) {
+		restler.get(program.url).on('complete', function(result){
+			if(result instanceof Error) {
+				console.error("%s is invalid, cannot be found. Exiting.", program.url);
+				process.exit(1);
+			}
+			var checkJson = checkHtmlFile(result, program.checks);
+			var outJson = JSON.stringify(checkJson, null, 4);
+			console.log(outJson);
+			goFlag = true;
+		});
+	}else {
+		htmlfile = fs.readFileSync(program.file);
+		var checkJson = checkHtmlFile(htmlfile, program.checks);
+		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}
+	
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
